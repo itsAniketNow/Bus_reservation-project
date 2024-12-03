588fa8d00f34a070e1e6060d602f6c83a0c210ea
Aniket Surwade <87859342+itsAniketNow@users.noreply.github.com>
2024-12-03 14:53:10 +0530

    Update settings.py

--- bus_reservation/bus_reservation/settings.py
+++ bus_reservation/bus_reservation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3nc=9lr5igxzqaknyz%cl0vmt&o293!##y55h2)p+q&a5srema
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
